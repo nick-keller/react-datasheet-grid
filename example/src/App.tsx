77f3e1befd0584e9c97043991401757d7442cc00
nick-keller <nicolas.kllr@gmail.com>
2021-06-23 20:08:46 +0200

    example app

---
+++ example/src/App.tsx
@@ -0,0 +1,53 @@
+import React, { useMemo, useState } from 'react'
+import { DataSheetGrid, Column } from 'react-datasheet-grid'
+
+function App() {
+  const [data, setData] = useState([
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+  ])
+  const [count, setCount] = useState(0)
+  const columns = useMemo<Partial<Column<string>>[]>(
+    () => [{ title: 'First name' }, {}, {}, {}, {}, {}],
+    []
+  )
+
+  return (
+    <div
+      style={{
+        margin: '50px',
+        padding: '50px',
+        maxWidth: '500px',
+        background: '#f3f3f3',
+      }}
+    >
+      <p>
+        Counter: <b>{count}</b>
+      </p>
+      <button onClick={() => setCount(count + 1)}>Up</button>
+      <DataSheetGrid
+        data={data}
+        headerRowHeight={100}
+        onChange={setData}
+        columns={columns}
+        stickyRightColumn={{}}
+      />
+    </div>
+  )
+}
+
+export default App
