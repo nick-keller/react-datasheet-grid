d104d8fa71145b203e0365401c5783b2d6914d0f
nick-keller <nicolas.kllr@gmail.com>
2022-05-20 09:30:35 +0200

    Add announcementBar

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -12,6 +12,11 @@ module.exports = {
   organizationName: 'Equify', // Usually your GitHub org/user name.
   projectName: 'react-datasheet-grid', // Usually your repo name.
   themeConfig: {
+    announcementBar: {
+      content:
+        '⭐️ <b>If you like React-Datasheet-Grid, give it a star on <a target="_blank" href="https://github.com/Equify/react-datasheet-grid">GitHub</a> and follow us on <a target="_blank" href="https://twitter.com/NicolasKa3">Twitter</a>!</b>',
+      backgroundColor: '#8bd6c4',
+    },
     navbar: {
       title: 'React Datasheet Grid',
       items: [
